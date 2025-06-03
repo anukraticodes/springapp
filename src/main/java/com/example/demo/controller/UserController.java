b28ba32b770b488696ea3c3b3c84d36a97e300f4
anukraticodes <chaturvedianukrati4@gmail.com>
2025-06-03 08:58:30 +0530

    initial message

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entity.User;
+import com.example.demo.repo.UserRepo;
+
+import org.springframework.ui.Model;
+
+@Controller
+
+public class UserController {
+	@Autowired(required = true)
+	private UserRepo repo;
+	
+	@GetMapping("/")
+	public String login(Model model) {
+		User user=new User();
+		model.addAttribute("user", user);
+		return "login";
+	}
+	@PostMapping("/userLogin")
+	public String loginUser(@ModelAttribute("user") User user) {
+		String userId=user.getUserId();
+		Optional<User> userdata=repo.findById(userId);
+		if(user.getPassword().equals(userdata.get().getPassword())) {
+		return "home";
+		}
+		else {
+			return "error";
+		}
+	}
+
+}
---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,34 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="user")
+
+public class User {
+    @Id
+	private String userId;
+	private String password;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
